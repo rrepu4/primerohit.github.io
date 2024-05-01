31687192d268d7305d90d47d05506aca045be505
primerohit <168153135+primerohit@users.noreply.github.com>
2024-05-01 16:28:40 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
+<script>
+  const showButtons = document.querySelectorAll('.show');
+  const interestsDivs = document.querySelectorAll('.hide');
 
+  showButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      interestsDivs[index].classList.toggle('hidden');
+      if (interestsDivs[index].classList.contains('hidden')) {
+        button.textContent = 'Show Responsibilites';
+      } else {
+        button.textContent = 'Hide Responsibities';
+      }
+    });
+  });
+</script>
